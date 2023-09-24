09ce3db4441ae989528148530131c2a0fe465181
theskyworld <2456450687@qq.com>
2023-09-24 22:00:07 +0800

    feat(Button) : add component & types

---
+++ src/components/Button/index.vue
@@ -0,0 +1,60 @@
+<script setup lang='ts'>
+import { ButtonProps } from './types';
+
+
+
+
+
+/*********************************导包分界线***************************************/
+/* macros */
+// 修改当前组件的对外导出组件名
+defineOptions({
+    name: "UButton",
+});
+
+
+/* props */
+const props = withDefaults(defineProps<ButtonProps>(), {
+    nativeType: 'button',
+})
+/* emits */
+
+
+/* datas */
+
+
+
+/* computed */
+
+
+
+/* methods */
+
+
+
+/* watch */
+
+
+/* hooks */
+
+/* useComposition */
+
+</script>
+<template>
+    <!-- 添加button元素的固定样式和动态样式，其中动态样式包含固定的样式名和动态样式名两种 -->
+    <!-- 同时设置button元素原生属性disabled、autofocus和type的值 -->
+    <button ref="_ref" class="u-button" :class="{
+        [`u-button--${type}`]: type,
+        [`u-button--${size}`]: size,
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle,
+        'is-disabled': disabled,
+        'is-loading': loading,
+    }" :disabled="disabled || loading" :autofocus="autofocus" :type="nativeType">
+        <span>
+            <slot />
+        </span>
+    </button>
+</template>
+<style scoped></style>
---
+++ src/components/Button/types.ts
@@ -0,0 +1,20 @@
+// Button组件的属性类型
+export interface ButtonProps {
+  type?: ButtonTypes;
+  size?: ButtonSizes;
+  plain?: boolean;
+
+  // 圆角相关
+  round?: boolean;
+  circle?: boolean;
+
+  disabled?: boolean;
+  icon?: string;
+  loading?: boolean;
+  // button元素内置原生属性
+  nativeType?: NativeType; // nativeType即原生button元素的type属性，与这里的type进行区分
+  autofocus?: boolean;
+}
+export type ButtonTypes = "primary" | "success" | "warning" | "danger" | "info";
+export type ButtonSizes = "large" | "small";
+export type NativeType = "button" | "submit" | "reset";
