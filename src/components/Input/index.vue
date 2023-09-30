4d7a6e28cea7748e004fb18dfecc38f97ecf0f1d
theskyworld <2456450687@qq.com>
2023-09-30 20:56:08 +0800

    feat(components) : add Input

---
+++ src/components/Input/index.vue
@@ -0,0 +1,49 @@
+<script setup lang='ts'>
+import { InputProps } from './types';
+
+
+
+
+
+/*********************************导包分界线***************************************/
+/* macros */
+defineOptions({
+    name: "UInput"
+})
+
+
+/* props */
+const props = withDefaults(defineProps<InputProps>(), {
+    type: "text",
+    placeholder: '请输入内容...'
+})
+/* emits */
+const emits = defineEmits(['update:modelValue'])
+
+/* datas */
+
+
+
+/* computed */
+
+
+
+/* methods */
+
+
+
+/* watch */
+
+
+/* hooks */
+
+/* useComposition */
+
+
+
+
+</script>
+<template>
+    <input :type="type" :placeholder="placeholder" :value="modelValue" @input="emits('update:modelValue', $event.target.value)" />
+</template>
+<style scoped></style>
---
+++ src/components/Input/types.ts
@@ -0,0 +1,9 @@
+export interface InputProps {
+  // 内置属性
+  type: string;
+  placeholder: string;
+  value?: any;
+
+  // 组件双向绑定
+  modelValue?: any;
+}
