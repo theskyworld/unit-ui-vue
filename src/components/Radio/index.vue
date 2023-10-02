ba47ec47785497d465edc5ffe9635bd0ac280748
theskyworld <2456450687@qq.com>
2023-10-02 15:54:51 +0800

    feat(components) : add Radio

---
+++ src/components/Radio/index.vue
@@ -0,0 +1,44 @@
+<script setup lang='ts'>
+import { RadioProps } from './typs';
+
+
+
+
+
+/*********************************导包分界线***************************************/
+/* macros */
+defineOptions({
+    name: 'URadio',
+})
+
+
+/* props */
+const props = withDefaults(defineProps<RadioProps>(), {})
+/* emits */
+const emits = defineEmits(['update:modelValue'])
+
+/* datas */
+
+
+
+/* computed */
+
+
+
+/* methods */
+
+
+
+/* watch */
+
+
+/* hooks */
+
+/* useComposition */
+
+</script>
+<template>
+    <input type="radio" :id="label" name="monster" :value="modelValue" />
+    <label :for="label"><span>{{ label }}</span></label>
+</template>
+<style scoped></style>
---
+++ src/components/Radio/typs.ts
@@ -0,0 +1,10 @@
+export interface RadioProps {
+  label?: string;
+  modelValue?: string;
+  name?: string;
+}
+
+// 用于获取那个被选中的单选按钮的值
+export interface RadioGroupProps {
+  modelValue?: string;
+}
