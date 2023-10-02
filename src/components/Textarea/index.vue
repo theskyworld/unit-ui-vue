c27b482fee1b4b3dc4a5ad3ddeb641aed83417d0
theskyworld <2456450687@qq.com>
2023-10-02 10:39:30 +0800

    feat(components) : add Textarea

---
+++ src/components/Textarea/index.vue
@@ -0,0 +1,41 @@
+<script setup lang='ts'>
+import { TextareaProps } from "./types";
+
+
+
+/*********************************导包分界线***************************************/
+/* macros */
+defineOptions({
+    name : 'UTextarea',
+})
+
+
+/* props */
+const props = withDefaults(defineProps<TextareaProps>(), {})
+/* emits */
+const emits = defineEmits(['update:modelValue'])
+
+/* datas */
+
+
+
+/* computed */
+
+
+
+/* methods */
+
+
+
+/* watch */
+
+
+/* hooks */
+
+/* useComposition */
+
+</script>
+<template>
+    <textarea :name=name cols="30" rows="10" :value="modelValue" @input="emits('update:modelValue', ($event.target as any).value)"></textarea>
+</template>
+<style scoped></style>
---
+++ src/components/Textarea/types.ts
@@ -0,0 +1,6 @@
+export interface TextareaProps {
+    name?: string;
+
+    // 双向绑定
+    modelValue?: string;
+}
